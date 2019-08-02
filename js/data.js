6e8517f4ce6636e2c3a9a628a853b013a0eacabe
Lukasz-Samsung <lukasz.sitnik@gmail.com>
2019-08-02 15:22:36 +0200

    Ordered cards array

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
 export const cards = [
     "janIIIsobieski.png",
+    "janIIIsobieski.png",
+    "kazimierz_wielki.png",
     "kazimierz_wielki.png",
     "przemyslII.png",
+    "przemyslII.png",
     "stefan_batory.png",
+    "stefan_batory.png",
+    "wladyslaw_jagiello.png",
     "wladyslaw_jagiello.png",
     "kazimierz_jagiellonczyk.png",
-    "kazimierz_wielki.png",
-    "przemyslII.png",
-    "stefan_batory.png",
-    "wladyslaw_jagiello.png",
-    "janIIIsobieski.png",
     "kazimierz_jagiellonczyk.png"
 ];
